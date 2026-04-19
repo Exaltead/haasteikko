27413b80dea39914d832f7de93f9e11a10abc758
exaltead <quex0v8gh@mozmail.com>
2026-04-19 17:42:59 +0300

    fix: fixed ordering number

--- packages/frontend/src/views/ChallengeSolutionView.vue
+++ packages/frontend/src/views/ChallengeSolutionView.vue
@@ -365,7 +365,7 @@ function updateSolutionForQuestion(questionId: string, newValue: string | string
                   <div v-else
                     class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 w-full max-w-[84rem] mx-auto px-2 md:px-0">
                     <LibraryItemCard v-for="(item, index) in challengeLibraryItems" :key="item.id" :item="item"
-                      :orderingNumber="index + 1" @itemUpdated="loadData" />
+                      :orderingNumber="challengeLibraryItems.length - index" @itemUpdated="loadData" />
                   </div>
                 </div>
               </template>
